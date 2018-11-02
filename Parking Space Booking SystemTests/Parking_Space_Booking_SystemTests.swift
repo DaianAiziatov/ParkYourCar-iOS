353d06115f2943152e6284999cc6efcd38178913
Tasneem Bohra <tasneem.bohra@ca-tor-wkst71pc.local>
2018-11-01 20:10:32 -0400

    Initial Commit

---
+++ Parking Space Booking SystemTests/Parking_Space_Booking_SystemTests.swift
@@ -0,0 +1,34 @@
+//
+//  Parking_Space_Booking_SystemTests.swift
+//  Parking Space Booking SystemTests
+//
+//  Created by Tasneem Bohra on 2018-11-01.
+//  Copyright © 2018 Lambton. All rights reserved.
+//
+
+import XCTest
+@testable import Parking_Space_Booking_System
+
+class Parking_Space_Booking_SystemTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
