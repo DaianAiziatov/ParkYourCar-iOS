fcfa6fb5007b635fa90abb454b0d83b73d800c40
Daian <daian.aiziatov@gmail.com>
2018-11-05 19:15:56 -0500

    finally resolved conflict

---
+++ Parking Space Booking System/SplashViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SplashViewController.swift
+//  Parking Space Booking System
+//
+//  Created by Daian Aiziatov on 05/11/2018.
+//  Copyright © 2018 Lambton. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(self.splashTimeOut(sender:)), userInfo: nil, repeats: false)
+    }
+    
+    @objc func splashTimeOut(sender : Timer) {
+        // TODO: Check if the user is already loggedin then direct to Home instead Login
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: "loginVC")
+        
+        do {
+            try (UIApplication.shared.delegate as! AppDelegate).window?.rootViewController = vc
+        } catch {
+            
+        }
+        
+    }
+    
+    
+}
