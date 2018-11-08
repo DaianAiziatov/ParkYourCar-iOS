ccfca16d7558d4ecbc881db806803d403c921369
Daian <daian.aiziatov@gmail.com>
2018-11-08 13:40:43 -0500

    starting implimentation of firebase

--- Parking Space Booking System/Controllers/LoginViewController.swift
+++ Parking Space Booking System/Controllers/LoginViewController.swift
@@ -24,14 +24,14 @@ class LoginViewController: UIViewController {
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        self.navigationController?.setNavigationBarHidden(true, animated: true)
-    }
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
-        self.navigationController?.setNavigationBarHidden(false, animated: true)
-    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(true)
+//        self.navigationController?.setNavigationBarHidden(true, animated: true)
+//    }
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(true)
+//        self.navigationController?.setNavigationBarHidden(false, animated: true)
+//    }
 
     @IBAction func login(_ sender: UIButton) {
         if isUserValid() {
---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Parking Space Booking System' do
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+end
